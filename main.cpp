1c2c3566a68907ad5e6ae5c6f3d3a5f4118ef764
Roberto Stagi <r.stagi96@gmail.com>
2019-04-25 15:38:28 +0200

    aggiornata gestione costanti

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-#include "rovmqtt.h"
+#include "PolitoceanConstants.h"
 #include <Publisher.h>
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
 
 using namespace std;
 using namespace cv;
+using namespace Politocean::Constants;
 
 int main(){
   
-  Publisher camera_publisher(HOST_ROV,PUBLISHERID); 
+  Publisher camera_publisher(MicroRov::HOST_ADDRESS, MicroRov::PUBLISHERID); 
   camera_publisher.connect();
   sleep(3);
   vector <uchar> encoded;
   VideoCapture cap(0);
   if(!cap.isOpened()){
-    camera_publisher.publish(ERROR_TOPIC,CAMERA_ERROR);
+    camera_publisher.publish(Topics::ERRORS, MicroRov::CAMERA_ERROR);
     return -1;
   }   
   while(1){
@@ -24,7 +25,7 @@ int main(){
     if(frame.size().width==0)continue;
     imencode(".jpg", frame, encoded);
     string str(encoded.begin(),encoded.end());
-    camera_publisher.publish(CAMERA_TOPIC,str);
+    camera_publisher.publish(Topics::MICRO_ROV_CAMERA, str);
     sleep(.1);
     char c=(char)waitKey(25);
     if(c==27)
