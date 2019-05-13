a188ba00e75ea3d402ff977a0af447a601e925fa
Gabriele Iurlaro <gabbiurlaro00@gmail.com>
2019-05-13 17:35:47 +0200

    added lib

---
+++ libs/Controller/Controller.h
@@ -0,0 +1,77 @@
+/**
+ * @author: pettinz
+ */
+
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+namespace Politocean
+{
+    namespace RPi
+    {
+        class Pinout
+        {
+        public:
+            static const int RESET              = 7;
+            static const int MOTORS             = 12;
+            
+            static const int SHOULDER_EN        = 15;
+            static const int SHOULDER_DIR       = 13;
+            static const int SHOULDER_STEP      = 11;
+            
+            static const int WRIST_EN           = 40;
+            static const int WRIST_DIR          = 38;
+            static const int WRIST_STEP         = 36;
+            
+            static const int HAND_DIR           = 16;
+            static const int HAND_PWM           = 18;
+            
+            static const int CAMERA_EN          = 33;
+            static const int CAMERA_DIR         = 31;
+            static const int CAMERA_STEP        = 29;
+        };
+        
+        class Controller
+        {
+            int spiDevice_;
+            
+            bool motors_;
+        
+        public:
+            enum class PinLevel { PIN_LOW, PIN_HIGH };
+            enum class PinMode { PIN_OUTPUT, PIN_INPUT };
+            
+            static const int DEFAULT_SPI_CHANNEL    = 0;
+            static const int DEFAULT_SPI_SPEED      = 1000000;
+            
+            Controller() : motors_(false) {}
+            
+            void setup();
+
+            void pinMode(int pin, PinMode mode);
+            void digitalWrite(int pin, PinLevel level);
+            
+            void softPwmCreate(int pwmPin, int start, int stop);
+            void softPwmWrite(int pwmPin, int value);
+            void softPwmStop(int pwmPin);
+            
+            void setupSPI(int device, int frequency);
+            void setupMotors();
+            
+            /**
+             * It returns the value read from SPI
+             * 
+             * @data : value to send via SPI
+             */
+            unsigned char SPIDataRW(unsigned char data);
+            
+            void switchMotors();
+            
+            // It resets the controller
+            void reset();
+        };
+    }
+}
+
+
+#endif //CONTROLLER_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "PolitoceanConstants.h"
 #include <Publisher.h>
+#include <Subscriber.h>
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
+//#include <wiringPi.h>
+//#include <softPwm.h>
+#include <PwmMotor.h>
+#include <Controller.h>
 
 using namespace std;
 using namespace cv;
@@ -10,28 +15,48 @@ using namespace Politocean;
 using namespace Politocean::Constants;
 using namespace Politocean::Constants::MicroRov;
 
+bool action,vel;
+
+void set_vel(const std::string& velocity){
+  return;
+}
+
+void set_action(const std::string& action){
+  return;
+}
+
 int main(){
-  
-  Publisher camera_publisher(MicroRov::HOST_ADDRESS, MicroRov::PUBLISHERID); 
+  RPi::Controller ctrl1;
+  RPi::Controller ctrl2;
+  RPi::PwmMotor motor1(&ctrl1,0,1,1,1);
+  RPi::PwmMotor motor2(&ctrl2,0,2,2,2);
+  Publisher camera_publisher(Constants::Hmi::IP_ADDRESS, Constants::MicroRov::MICRO_ROV_ID);
+  Subscriber MotorSubscriber(Constants::Rov::IP_ADDRESS, Constants::MicroRov::MICRO_ROV_ID);
+  MotorSubscriber.subscribeTo(Constants::Topics::MICROROV_COMMANDS, &set_action);
+  MotorSubscriber.subscribeTo(Constants::Topics::MICROROV_VELOCITY, &set_vel);
+  MotorSubscriber.connect();
   camera_publisher.connect();
-  sleep(3);
   vector <uchar> encoded;
   VideoCapture cap(0);
   if(!cap.isOpened()){
-    camera_publisher.publish(Common::Topics::ERRORS, "Camera error");
+    camera_publisher.publish(Constants::Topics::ERRORS, "Camera error");
     return -1;
   }   
-  while(camera_publisher.is_connected()){
-    Mat frame, decoded_frame;
-    cap >> frame;
-    if(frame.size().width==0)continue;
-    imencode(".jpg", frame, encoded);
-    string str(encoded.begin(),encoded.end());
-    camera_publisher.publish(Topics::MICRO_ROV_CAMERA, str);
-    sleep(.1);
-    char c=(char)waitKey(25);
-    if(c==27)
-      break;
+  while(MotorSubscriber.is_connected()){
+    if(camera_publisher.is_connected()){
+      Mat frame, decoded_frame;
+      cap >> frame;
+      if(frame.size().width==0)continue;
+      imencode(".jpg", frame, encoded);
+      string str(encoded.begin(),encoded.end());
+      camera_publisher.publish(Topics::MICRO_ROV_CAMERA, str);
+      sleep(.1);
+      char c=(char)waitKey(25);
+      if(c==27)
+        break;
+      
+    }
+    //
   }
 }
 
