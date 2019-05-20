6099b3e150dffeb14a4902f2eeb3a04b19748aba
Gabriele Iurlaro <gabbiurlaro00@gmail.com>
2019-05-20 17:53:45 +0200

    added files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< Updated upstream
 #include "rovmqtt.h"
 #include <Publisher.h>
 #include <opencv2/opencv.hpp>
@@ -10,6 +11,53 @@ using namespace cv;
 int main(){
   
   Publisher camera_publisher(HOST_ROV,PUBLISHERID); 
+=======
+#include <string>
+#include "PolitoceanConstants.h"
+#include "Publisher.h"
+#include "Subscriber.h"
+#include <opencv2/opencv.hpp>
+#include <unistd.h>
+#include "PwmMotor.h"
+#include "Controller.h"
+
+using namespace std;
+using namespace cv;
+using namespace Politocean;
+using namespace Politocean::RPi;
+using namespace Politocean::Constants;
+using namespace Politocean::Constants::MicroRov;
+
+Controller ctrl;
+PwmMotor motor1(&ctrl,0,1,1,1); // int dirPin - PwmPin - minPwm - maxPwm
+PwmMotor motor2(&ctrl,0,2,2,2); // Bisogna inserire i valori sopra;
+
+motor1.setup();
+motor2.setup();
+
+void set_vel(const std::string& velocity){
+  return;
+}
+
+void set_action(const std::string& action){
+  if(action.compare("start")){
+    motor1.startPwm();
+    motor2.startPwm();
+  }
+  else if(action.compare("stop")){
+    motor1.stopPwm();
+    motor2.stopPwm();
+  }
+  return;
+}
+
+int main(){
+  Publisher camera_publisher(Constants::MicroRov::IP_ADDRESS, Constants::MicroRov::MICRO_ROV_ID);
+  Subscriber MotorSubscriber(Constants::MicroRov::IP_ADDRESS, Constants::MicroRov::MICRO_ROV_ID);
+  MotorSubscriber.subscribeTo(Constants::Topics::MICROROV_COMMANDS, &set_action);
+  MotorSubscriber.subscribeTo(Constants::Topics::MICROROV_VELOCITY, &set_vel);
+  MotorSubscriber.connect();
+>>>>>>> Stashed changes
   camera_publisher.connect();
   sleep(3);
   vector <uchar> encoded;
@@ -18,6 +66,7 @@ int main(){
     camera_publisher.publish(ERROR_TOPIC,CAMERA_ERROR);
     return -1;
   }   
+<<<<<<< Updated upstream
   while(1){
     Mat frame, decoded_frame;
     cap >> frame;
@@ -29,6 +78,21 @@ int main(){
     char c=(char)waitKey(25);
     if(c==27)
       break;
+=======
+  while(MotorSubscriber.is_connected()){
+    if(camera_publisher.is_connected()){
+      Mat frame, decoded_frame;
+      cap >> frame;
+      if(frame.size().width==0)continue;
+      imencode(".jpg", frame, encoded);
+      string str(encoded.begin(),encoded.end());
+      camera_publisher.publish(Topics::MICRO_ROV_CAMERA, str);
+      sleep(.1);
+      char c=(char)waitKey(25);
+      if(c==27)
+        break;
+    }
+>>>>>>> Stashed changes
   }
 }
 
